f5b6f8391fe51f90637cba75b5dccd18ee185160
KiulWasTaken <pvpkylecr07@gmail.com>
2023-06-23 10:01:49 +0930

    added particles for all status effect methods

--- src/main/java/kiul/kiulabilities/StatusEffects.java
+++ src/main/java/kiul/kiulabilities/StatusEffects.java
@@ -11,6 +11,7 @@ import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 
@@ -20,15 +21,50 @@ public class StatusEffects implements Listener {
 
 
     static void stun(Player p, int time) {
+        Plugin plugin = Kiulabilities.getPlugin(Kiulabilities.class);
         p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, time, 1, true, false));
         p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, time, 1, true, false));
         p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, time, 1, true, false));
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new BukkitRunnable() {
+            int i = time * 2;
+
+            @Override
+            public void run() {
+
+                if (i > 0) {
+                    i--;
+                    p.getWorld().spawnParticle(Particle.REDSTONE, p.getEyeLocation().add(0, 0.5, 0), 5, new Particle.DustOptions(Color.YELLOW, 1));
+
+                } else {
+                    cancel();
+                }
+
+            }
+        }, 0L, 10);
     }
 
     static void root(Player p, int time) {
         Plugin plugin = Kiulabilities.getPlugin(Kiulabilities.class);
 
         preventMove.add(p);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new BukkitRunnable() {
+            int i = time * 2;
+
+            @Override
+            public void run() {
+
+                if (i > 0) {
+                    i--;
+                    p.getWorld().spawnParticle(Particle.REDSTONE, p.getEyeLocation().add(0, 0.5, 0), 5, new Particle.DustOptions(Color.PURPLE, 1));
+
+                } else {
+                    cancel();
+                }
+
+            }
+        }, 0L, 10);
         Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
             @Override
             public void run() {
@@ -39,6 +75,22 @@ public class StatusEffects implements Listener {
 
     static void shield(Player p, int level, int time) {
         Plugin plugin = Kiulabilities.getPlugin(Kiulabilities.class);
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new BukkitRunnable() {
+            int i = time * 2;
+
+            @Override
+            public void run() {
+
+                if (i > 0) {
+                    i--;
+                    p.getWorld().spawnParticle(Particle.REDSTONE, p.getEyeLocation().add(0, 0.5, 0), 5, new Particle.DustOptions(Color.TEAL, 1));
+
+                } else {
+                    cancel();
+                }
+
+            }
+        }, 0L, 10);
 
         p.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, time, level, true, false));
 
@@ -50,6 +102,23 @@ public class StatusEffects implements Listener {
 
         p.setSaturatedRegenRate(-1);
         p.setUnsaturatedRegenRate(-1);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new BukkitRunnable() {
+            int i = time * 2;
+
+            @Override
+            public void run() {
+
+                if (i > 0) {
+                    i--;
+                    p.getWorld().spawnParticle(Particle.REDSTONE, p.getEyeLocation().add(0, 0.5, 0), 5, new Particle.DustOptions(Color.RED, 1));
+
+                } else {
+                    cancel();
+                }
+
+            }
+        }, 0L, 10);
         Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
             @Override
             public void run() {
@@ -58,8 +127,6 @@ public class StatusEffects implements Listener {
             }
         }, time * 20);
     }
-
-    private int taskId;
 
 
     @EventHandler
